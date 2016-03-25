1c95678eba0931a43a85d0c557cb08b1d38844a7
seif <seif@seif-PC>
2016-03-25 20:31:21 +0200

    adjusting settings for compiling

--- SOFTWARE_ENGINEERING_PROJECT/SOFTWARE/HAL_LAYER/LCD/LCD_CODE/LCD_config.h
+++ SOFTWARE_ENGINEERING_PROJECT/SOFTWARE/HAL_LAYER/LCD/LCD_CODE/LCD_config.h
@@ -10,20 +10,20 @@
 /***************************************************************************/
 /* Comment!: Data pins*/
 /* Range!: DIO_u8PIN0 ~ DIO_u8PIN31 */
-#define LCD_u8D0	DIO_u8PIN0
-#define LCD_u8D1	DIO_u8PIN1
-#define LCD_u8D2	DIO_u8PIN2
-#define LCD_u8D3	DIO_u8PIN3
-#define LCD_u8D4	DIO_u8PIN4
-#define LCD_u8D5	DIO_u8PIN5
-#define LCD_u8D6	DIO_u8PIN6
-#define LCD_u8D7	DIO_u8PIN7
+#define LCD_u8D0	DIO_u8PIN16
+#define LCD_u8D1	DIO_u8PIN17
+#define LCD_u8D2	DIO_u8PIN18
+#define LCD_u8D3	DIO_u8PIN19
+#define LCD_u8D4	DIO_u8PIN20
+#define LCD_u8D5	DIO_u8PIN21
+#define LCD_u8D6	DIO_u8PIN22
+#define LCD_u8D7	DIO_u8PIN23
 
 /* Comment!: control pins*/
 /* Range!: DIO_u8PIN0 ~ DIO_u8PIN31 */
-#define LCD_u8RS		DIO_u8PIN8
-#define LCD_u8RW		DIO_u8PIN9
-#define LCD_u8ENABLE	DIO_u8PIN10
+#define LCD_u8RS		DIO_u8PIN24
+#define LCD_u8RW		DIO_u8PIN25
+#define LCD_u8ENABLE	DIO_u8PIN26
 
 /* Comment!: initial state */
 /* Range!: LCD_u8ON ~ LCD_u8OFF*/
